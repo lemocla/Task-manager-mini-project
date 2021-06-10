9b4e41daaea2eadfa1f0b69bdd7266f6dfa1ee96
lemocla <lemocla@icloud.com>
2021-06-10 13:29:31 +0000

    Add login functionality

--- app.py
+++ app.py
@@ -32,9 +32,8 @@ def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
 
+
 # registration functionality
-
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -45,7 +44,7 @@ def register():
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
-            
+
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
@@ -57,6 +56,34 @@ def register():
         flash("Registration successful!")
 
     return render_template("register.html")
+
+
+# login functionality
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check for existing user
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # check if passowrd matches
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
