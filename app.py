a3a38ed6250389e9cfc30b95dc6125e60fa7b296
lemocla <lemocla@icloud.com>
2021-06-11 13:18:13 +0000

    Add Search functionality

--- app.py
+++ app.py
@@ -32,6 +32,12 @@ def get_tasks():
     tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    tasks = list(mongo.db.tasks.find({"$text": {"$search": query}}))
+    return render_template("tasks.html", tasks=tasks)
+
 
 # registration functionality
 @app.route("/register", methods=["GET", "POST"])
